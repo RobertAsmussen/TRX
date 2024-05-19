fb216953f66710bc26444e0f42337e66935f1546
Robert.Asmussen <st169586@stud.uni-stuttgart.de>
2024-05-19 10:48:25 +0200

    New Dataset and Split for Hyperparameter tuning

--- file.py
+++ file.py
@@ -1,8 +1,8 @@
 import os
 import random
+import shutil
 
-
-def CreateSplit(src_path, dst_path, train_class_list, val_class_list, test_class_list):
+def CreateSplit(src_path, dst_path, train_class_list, val_class_list, test_class_list, max_number_videos_test=-1, max_number_videos_train=-1):
     video_count = 0
     count = 1
     train_txt_path = os.path.join(dst_path, "trainlist01.txt")
@@ -18,23 +18,36 @@ def CreateSplit(src_path, dst_path, train_class_list, val_class_list, test_class
     with open(train_txt_path, "w") as file:
         for c in train_class_list:
             c_path = os.path.join(src_path, c)
+            videos_per_class_count = 0
             for video in os.listdir(c_path):
                 file.write(f"{c}/{video}\n")
                 video_count += 1
+                videos_per_class_count += 1
+                if(max_number_videos_train >= 0 and videos_per_class_count >= max_number_videos_train):
+                    break
 
     with open(val_txt_path, "w") as file:
         for c in val_class_list:
             c_path = os.path.join(src_path, c)
+            videos_per_class_count = 0
             for video in os.listdir(c_path):
                 file.write(f"{c}/{video}\n")
                 video_count += 1
+                videos_per_class_count += 1
+                if(max_number_videos_test >= 0 and videos_per_class_count >= max_number_videos_test):
+                    break
 
     with open(test_txt_path, "w") as file:
         for c in test_class_list:
             c_path = os.path.join(src_path, c)
+            videos_per_class_count = 0
             for video in os.listdir(c_path):
                 file.write(f"{c}/{video}\n")
+                videos_per_class_count += 1
+                if(max_number_videos_test >= 0 and videos_per_class_count >= max_number_videos_test):
+                    break
                 video_count += 1
+
     print(f"Train Split created: {train_txt_path}")
     print(f"Validation Split created: {val_txt_path}")
     print(f"Test Split created: {test_txt_path}")
@@ -94,8 +107,39 @@ def createSplit_fixedSurgeries(src_path, train_surgeries_prefix, val_surgeries_p
     
     return train_class_list, val_class_list, test_class_list
 
+def createDataset(train_txt_path, val_txt_path, test_txt_path, src_path, dst_path):
+    def copy_folders(txt_path, split_name):
+        with open(txt_path, 'r') as file:
+            lines = file.readlines()
+        
+        for line in lines:
+            relative_path = line.strip()
+            src_folder = os.path.join(src_path, relative_path)
+            dst_folder = os.path.join(dst_path, split_name, relative_path)
+            
+            # Ensure the destination directory exists
+            os.makedirs(os.path.dirname(dst_folder), exist_ok=True)
+            
+            # Copy the folder
+            if os.path.exists(src_folder):
+                shutil.copytree(src_folder, dst_folder)
+                print(f"Copy {src_folder} to {dst_folder}")
+            else:
+                print(f"Source folder {src_folder} does not exist")
+
+    # Copy folders for each split
+    copy_folders(train_txt_path, 'train')
+    copy_folders(val_txt_path, 'val')
+    copy_folders(test_txt_path, 'test')
+
 if __name__ == "__main__":
-    src_directory = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\\TRX\\video_datasets\\data\\surgicalphasev1_Xx256"
-    dst_directory = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\\TRX\\video_datasets\\splits\\surgicalphasev1TrainTestlist"
-    train_list, val_list, test_list = createSplit_fixedSurgeries(src_directory, ["LH", "CA", "GR", "HC"], ["M2", "HCH"], ["C80"])
-    CreateSplit(src_directory, dst_directory, train_list, val_list, test_list)
+    #src_directory = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\\TRX\\video_datasets\\data\\surgicalphasev1_Xx256"
+    #dst_directory = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\\TRX\\video_datasets\\splits\\surgicalphasev2TrainTestlist"
+    #train_list, val_list, test_list = createSplit_fixedSurgeries(src_directory, ["CA"], [], ["C80"])
+    #CreateSplit(src_directory, dst_directory, train_list, val_list, test_list, 11, 15)
+    src = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\\TRX\\video_datasets\\data\\surgicalphasev1_Xx256"
+    dst = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\\TRX\\video_datasets\\data\\surgicalphasev2_Xx256"
+    train = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\TRX\\video_datasets\\splits\\surgicalphasev2TrainTestlist\\trainlist01.txt"
+    test = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\TRX\\video_datasets\\splits\\surgicalphasev2TrainTestlist\\testlist01.txt"
+    val = "G:\\Meine Ablage\\Studium\\Master\\Forschungsarbeit\\05_Data\TRX\\video_datasets\\splits\\surgicalphasev2TrainTestlist\\vallist01.txt"
+    createDataset(train, val, test, src, dst)
